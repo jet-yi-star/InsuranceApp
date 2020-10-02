fbfc1f7d496769f8e0393b223cad94cc0c2bfcec
jet-yi-star <72280090+jet-yi-star@users.noreply.github.com>
2020-10-02 16:33:07 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // template:'<H1>{{message}} </H1>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo';
+  message = 'jet no 1';
+}
